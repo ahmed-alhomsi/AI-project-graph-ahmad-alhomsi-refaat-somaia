b0117ef2013d061be5200db131f1c0bc17bdac7c
ahmed alhomsi <ahmed.alhomsi.official@gmail.com>
2023-12-31 17:12:51 +0300

    made sure proper datastructures are used, and that paths are logged to console.

--- SecondPart.js
+++ SecondPart.js
@@ -1,190 +1,220 @@
+class Stack {
+  constructor() {
+      this.items = [];
+  }
+
+  push(item) {
+      this.items.push(item);
+  }
+
+  pop() {
+      return this.items.pop();
+  }
+
+  isEmpty() {
+      return this.items.length === 0;
+  }
+}
+
+class Queue {
+  constructor() {
+      this.items = [];
+  }
+
+  enqueue(item) {
+      this.items.push(item);
+  }
+
+  dequeue() {
+      return this.items.shift();
+  }
+
+  isEmpty() {
+      return this.items.length === 0;
+  }
+}
+
+class PriorityQueue {
+  constructor(comparator) {
+      this.items = [];
+      this.comparator = comparator || ((a, b) => a.priority - b.priority);
+  }
+
+  enqueue(item, priority) {
+      this.items.push({ item, priority });
+      this.items.sort(this.comparator);
+  }
+
+  dequeue() {
+      return this.items.shift().item;
+  }
+
+  isEmpty() {
+      return this.items.length === 0;
+  }
+}
+
 const graph = {
-    S: { G: 9, A: 2, B: 1 },
-    A: { C: 2, D: 3 },
-    B: { D: 2, E: 4 },
-    C: { G: 4 },
-    D: { G: 4 },
-    E: {},
-    G: {}
-  };
-  
-  function getHeuristic(node) {
+  S: { G: 9, A: 2, B: 1 },
+  A: { C: 2, D: 3 },
+  B: { D: 2, E: 4 },
+  C: { G: 4 },
+  D: { G: 4 },
+  E: {},
+  G: {}
+};
 
-    const heuristicValues = { 
-        S: 6, 
-        A: 0, 
-        B: 6, 
-        C: 4, 
-        D: 1, 
-        E: 10, 
-        G: 0 
-    };
+function dfs(graph, start, goal) {
+  const stack = new Stack();
+  const visited = new Set();
 
-    return heuristicValues[node];
-  }
-  
-  const startNode = 'S';
-  const goalNode = 'G';
+  stack.push({ node: start, path: [start], cost: 0 });
 
-  function dfs(graph, start, goal, visited = new Set()) {
-    if (start === goal) {
-      return { path: [start], cost: 0 };
-    }
-  
-    visited.add(start);
-  
-    for (const neighbor in graph[start]) {
-      if (!visited.has(neighbor)) {
-        const result = dfs(graph, neighbor, goal, visited);
-        if (result.path.length > 0) {
-          result.path.unshift(start);
-          result.cost += graph[start][neighbor];
-          return result;
-        }
-      }
-    }
-  
-    return { path: [], cost: Infinity };
-  }
- 
-  
-  function bfs(graph, start, goal) {
-    const queue = [{ node: start, path: [start], cost: 0 }];
-    const visited = new Set();
-  
-    while (queue.length > 0) {
-      const { node, path, cost } = queue.shift();
-  
+  while (!stack.isEmpty()) {
+      const { node, path, cost } = stack.pop();
+
       if (node === goal) {
-        return { path, cost };
+          return { path, cost };
       }
-  
+
       visited.add(node);
-  
-      for (const neighbor in graph[node]) {
-        if (!visited.has(neighbor)) {
-          queue.push({
-            node: neighbor,
-            path: [...path, neighbor],
-            cost: cost + graph[node][neighbor]
-          });
-          visited.add(neighbor);
-        }
-      }
-    }
-  
-    return { path: [], cost: Infinity };
-  }
 
-
-  function ucs(graph, start, goal) {
-    const queue = [{ node: start, path: [start], cost: 0 }];
-    const visited = new Set();
-  
-    while (queue.length > 0) {
-      queue.sort((a, b) => a.cost - b.cost);
-      const { node, path, cost } = queue.shift();
-  
-      if (node === goal) {
-        return { path, cost };
-      }
-  
-      visited.add(node);
-  
       for (const neighbor in graph[node]) {
-        if (!visited.has(neighbor)) {
-          const newCost = cost + graph[node][neighbor];
-          queue.push({
-            node: neighbor,
-            path: [...path, neighbor],
-            cost: newCost
-          });
-        }
-      }
-    }
-  
-    return { path: [], cost: Infinity };
-  }
-
-  function aStar(graph, start, goal, heuristic) {
-    const queue = [];
-    const cameFrom = {};
-    const gScore = {};
-    const fScore = {};
-  
-    gScore[start] = 0;
-    fScore[start] = heuristic(start);
-  
-    queue.push({ node: start, path: [start], cost: 0, heuristicValue: fScore[start] });
-  
-    while (queue.length > 0) {
-      queue.sort((a, b) => a.cost + a.heuristicValue - (b.cost + b.heuristicValue));
-      const { node, path, cost, heuristicValue } = queue.shift();
-  
-      if (node === goal) {
-        // console.log("Goal reached!");
-        // console.log(`A* Path from S to G: ${path.join(' -> ')}`);
-        // console.log(`Total Cost: ${cost}`);
-        // console.log(`Heuristic Value: ${heuristicValue}`);
-        return { path, cost: gScore[node], heuristicValue };
-      }
-  
-      for (const neighbor in graph[node]) {
-        const tentativeGScore = cost + graph[node][neighbor];
-  
-        if (!gScore[neighbor] || tentativeGScore < gScore[neighbor]) {
-          cameFrom[neighbor] = node;
-          gScore[neighbor] = tentativeGScore;
-          const priority = tentativeGScore + heuristic(neighbor) + 1;
-  
-          const neighborInfo = { node: neighbor, path: [...path, neighbor], cost: tentativeGScore, heuristicValue: heuristic(neighbor) };
-  
-          if (!queue.some(item => item.node === neighbor)) {
-            queue.push(neighborInfo);
-          } else {
-            const index = queue.findIndex(item => item.node === neighbor);
-            if (priority < queue[index].cost + queue[index].heuristicValue) {
-              queue[index] = neighborInfo;
-            }
+          if (!visited.has(neighbor)) {
+              stack.push({
+                  node: neighbor,
+                  path: [...path, neighbor],
+                  cost: cost + graph[node][neighbor]
+              });
           }
-        }
       }
-    }
-  
-    console.log("No path found.");
-    return { path: [], cost: Infinity, heuristicValue: Infinity };
   }
-  
-  for (const node in graph) {
-    const neighbors = Object.keys(graph[node]);
-    console.log(`${node} (h=${getHeuristic(node)}) connected to: ${neighbors.join(', ')}`);
-  }
-   
-  const dfsResult = dfs(graph, startNode, goalNode);
-  
-  // if (dfsResult.path.length > 0) {
-  //   console.log(`DFS Path from ${startNode} to ${goalNode}: ${dfsResult.path.join(' -> ')}`);
-  //   console.log(`Total Cost: ${dfsResult.cost}`);
-  // } else {
-  //   console.log(`No path found from ${startNode} to ${goalNode} using DFS.`);
-  // }
-  
-  const bfsResult = bfs(graph, startNode, goalNode);
-  
-  // if (bfsResult.path.length > 0) {
-  //   console.log(`BFS Path from ${startNode} to ${goalNode}: ${bfsResult.path.join(' -> ')}`);
-  //   console.log(`Total Cost: ${bfsResult.cost}`);
-  // } else {
-  //   console.log(`No path found from ${startNode} to ${goalNode} using BFS.`);
-  // }
 
-  const aStarResult = aStar(graph, 'S', 'G', getHeuristic); 
-    
-  const ucsResult = ucs(graph, startNode, goalNode);
-  
-  // if (ucsResult.path.length > 0) {
-  //   console.log(`UCS Path from ${startNode} to ${goalNode}: ${ucsResult.path.join(' -> ')}`);
-  //   console.log(`Total Cost: ${ucsResult.cost}`);
-  // } else {
-  //   console.log(`No path found from ${startNode} to ${goalNode} using UCS.`);
-  // }
+  return { path: [], cost: Infinity };
+}
+
+function bfs(graph, start, goal) {
+  const queue = new Queue();
+  const visited = new Set();
+
+  queue.enqueue({ node: start, path: [start], cost: 0 });
+
+  while (!queue.isEmpty()) {
+      const { node, path, cost } = queue.dequeue();
+
+      if (node === goal) {
+          return { path, cost };
+      }
+
+      visited.add(node);
+
+      for (const neighbor in graph[node]) {
+          if (!visited.has(neighbor)) {
+              queue.enqueue({
+                  node: neighbor,
+                  path: [...path, neighbor],
+                  cost: cost + graph[node][neighbor]
+              });
+
+              visited.add(neighbor);
+          }
+      }
+  }
+
+  return { path: [], cost: Infinity };
+}
+
+function ucs(graph, start, goal) {
+  const priorityQueue = new PriorityQueue((a, b) => a.cost - b.cost);
+  const visited = new Set();
+
+  priorityQueue.enqueue({ node: start, path: [start], cost: 0 });
+
+  while (!priorityQueue.isEmpty()) {
+      const { node, path, cost } = priorityQueue.dequeue();
+
+      if (node === goal) {
+          return { path, cost };
+      }
+
+      visited.add(node);
+
+      for (const neighbor in graph[node]) {
+          if (!visited.has(neighbor)) {
+              const newCost = cost + graph[node][neighbor];
+              priorityQueue.enqueue({
+                  node: neighbor,
+                  path: [...path, neighbor],
+                  cost: newCost
+              }, newCost);
+          }
+      }
+  }
+
+  return { path: [], cost: Infinity };
+}
+
+function aStar(graph, start, goal, heuristic) {
+  const priorityQueue = new PriorityQueue((a, b) => (a.cost + a.heuristicValue) - (b.cost + b.heuristicValue));
+  const visited = new Set();
+  const cameFrom = {};
+  const gScore = {};
+
+  gScore[start] = 0;
+  const startNodeInfo = {
+      node: start,
+      path: [start],
+      cost: 0,
+      heuristicValue: heuristic(start)
+  };
+  priorityQueue.enqueue(startNodeInfo, startNodeInfo.cost + startNodeInfo.heuristicValue);
+
+  while (!priorityQueue.isEmpty()) {
+      const { node, path, cost, heuristicValue } = priorityQueue.dequeue();
+
+      if (node === goal) {
+          return { path, cost: gScore[node], heuristicValue };
+      }
+
+      visited.add(node);
+
+      for (const neighbor in graph[node]) {
+          const tentativeGScore = cost + graph[node][neighbor];
+
+          if (!gScore[neighbor] || tentativeGScore < gScore[neighbor]) {
+              cameFrom[neighbor] = node;
+              gScore[neighbor] = tentativeGScore;
+              const priority = tentativeGScore + heuristic(neighbor);
+
+              const neighborInfo = {
+                  node: neighbor,
+                  path: [...path, neighbor],
+                  cost: tentativeGScore,
+                  heuristicValue: heuristic(neighbor)
+              };
+
+              if (!priorityQueue.items.some(item => item.node === neighbor)) {
+                  priorityQueue.enqueue(neighborInfo, priority);
+              } else {
+                  const index = priorityQueue.items.findIndex(item => item.node === neighbor);
+                  if (priority < priorityQueue.items[index].cost + priorityQueue.items[index].heuristicValue) {
+                      priorityQueue.items[index] = neighborInfo;
+                  }
+              }
+          }
+      }
+  }
+
+  console.log("No path found.");
+  return { path: [], cost: Infinity, heuristicValue: Infinity };
+}
+
+const dfsResult = dfs(graph, 'S', 'G');
+const bfsResult = bfs(graph, 'S', 'G');
+const ucsResult = ucs(graph, 'S', 'G');
+const aStarResult = aStar(graph, 'S', 'G', (node) => 0);
+
+console.log("DFS Result:", dfsResult);
+console.log("BFS Result:", bfsResult);
+console.log("UCS Result:", ucsResult);
+console.log("A* Result:", aStarResult);
